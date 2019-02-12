f4cfe5616dbbfa464d1c3534d39e08fc9f19e7e3
sunilnittian <47137520+sunilnittian@users.noreply.github.com>
2019-02-12 10:43:24 +0530

    update changes sunil

---
+++ Frontend/ERP/ERP/Controllers/CacheController.cs
@@ -0,0 +1,25 @@
+﻿using ERP.Models.Cache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Controllers
+{
+    public class CacheController : Controller
+    {
+        // GET: Cache
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void RefreshGridSettings()
+        {
+
+            GlobalCache gcObj = new GlobalCache();
+            gcObj.CreateDynamicGridJS();
+        }
+    }
+}
--- Frontend/ERP/ERP/Scripts/AppConfig.js
+++ Frontend/ERP/ERP/Scripts/AppConfig.js
@@ -2,4 +2,9 @@
     var Label_Header = new Array();
     Label_Header["$MONTH$"] = "Month";
     Label_Header["$ERP$"] = "eCampus";
+    Label_Header["$YEAR_CODE$"] = "Year Code";
+    Label_Header["$ACADEMICY_YEAR$"] = "Academic Year";
+    Label_Header["$WEF_DATE$"] = "Effective From";
+    Label_Header["$WET_DATE$"] = "Effectvie To";
+
 }
