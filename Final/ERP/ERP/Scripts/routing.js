20676db41eac315b9dc17698c7852233666f0ea1
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2019-04-01 23:15:03 +0530

    CHnages by VIshal

--- Final/ERP/ERP/Scripts/routing.js
+++ Final/ERP/ERP/Scripts/routing.js
@@ -6,38 +6,60 @@ $(function () {
 var routingApp = $.sammy("#MainContent", function () {
     this.get("#/Master/Academic/", function (context) {
         $.get("/Master/Academic", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
+            
             
         });
     });
     this.get("#/Master/Activity/", function (context) {
         $.get("/Master/Activity", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     this.get("#/Master/Course/", function (context) {
         $.get("/Master/Course", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     this.get("#/Master/Duration/", function (context) {
         $.get("/Master/Duration", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     this.get("#/Master/Mapping/", function (context) {
         $.get("/Master/Mapping", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     this.get("#/Master/Section/", function (context) {
         $.get("/Master/Section", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     this.get("#/Master/Subject/", function (context) {
         $.get("/Master/Subject", function (data) {
-            context.$element().html(data);
+            ShowLoading();
+            setTimeout(function () {
+                context.$element().html(data);
+            }, 1000);
         });
     });
     //this.get("#/Student/Add", function (context) {
@@ -73,8 +95,18 @@ var routingApp = $.sammy("#MainContent", function () {
     //});
 });
 $(function () {
-    //$.get("/Dashboard/Overview", function (data) {
-    //    context.$element().html(data);
-    //});
     routingApp.run("/Dashboard/Overview"); // default routing page.
 });
+function ShowLoading()
+{
+    if ($("#loading").length === 0) {
+        // inject the bar..
+        $("body").append($("<div><b></b><i></i></div>").attr("id", "loading"));
+
+        // animate the progress..
+        $("#loading").width("101%").delay(800).fadeOut(1000, function () {
+            // ..then remove it.
+            $(this).remove();
+        });
+    }
+}
